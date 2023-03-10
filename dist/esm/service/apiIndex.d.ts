9e5697196da26b83c2213e3e1b524cef68c64b5b
Ahmed Atef <43449355+ahmedatefdev@users.noreply.github.com>
2023-03-10 13:51:23 +0200

    add hasky

--- dist/esm/service/apiIndex.d.ts
+++
@@ -1,2 +0,0 @@
-declare const PrintSupport: any;
-export { PrintSupport };
--- dist/esm/service/apiIndex.js
+++
@@ -1,6 +0,0 @@
-/* tslint:disable variable-name */
-import api from "./api";
-import printSupport from "./printSupport";
-var PrintSupport = printSupport(api);
-export { PrintSupport };
-//# sourceMappingURL=apiIndex.js.map
